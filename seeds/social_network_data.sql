1ac7618ee977314d7b122cfdc4782316695394b3
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 14:58:18 +0000

    Splits data seed file into users / data for easier seeding

--- seeds/social_network_data.sql
+++ seeds/social_network_data.sql
@@ -1,12 +1,5 @@
 -- Seed users table with example data
 
-INSERT INTO users (username, email_address) VALUES ('jake', 'jake@email.com');
-INSERT INTO users (username, email_address) VALUES ('imogen', 'imogen@email.com');
-INSERT INTO users (username, email_address) VALUES ('jack', 'jack@email.com');
-INSERT INTO users (username, email_address) VALUES ('alec', 'alec@email.com');
-
--- Seed users table with example data
-
 INSERT INTO posts (title, content, user_id) VALUES ('An example title', 'Hello, today I went to the shops.', 1);
 INSERT INTO posts (title, content, user_id) VALUES ('Swimming pool times', 'Hello, today I went to the swimming pool.', 3);
 INSERT INTO posts (title, content, user_id) VALUES ('Tech news', 'A long blog post about a particualr computing problem.', 2);
---
+++ seeds/social_network_users.sql
@@ -0,0 +1,6 @@
+-- Seed users table with example data
+
+INSERT INTO users (username, email_address) VALUES ('jake', 'jake@email.com');
+INSERT INTO users (username, email_address) VALUES ('imogen', 'imogen@email.com');
+INSERT INTO users (username, email_address) VALUES ('jack', 'jack@email.com');
+INSERT INTO users (username, email_address) VALUES ('alec', 'alec@email.com');
