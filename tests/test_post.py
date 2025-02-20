eeb46f14786b111dfc49591d2b98062c076bf39e
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 13:28:51 +0000

    Updates post class tests

--- tests/test_post.py
+++ tests/test_post.py
@@ -2,26 +2,32 @@
 Test file for Post class in file: lib/post.py
 '''
 
-'''
-When I create a post
-It initialises correctly
-'''
-post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
-post_1.title == 'A test title'
-post_1.content == 'Test content'
-post_1.user_id == 1
+from lib.post import Post
 
-'''
-When I add two posts with identical attributes
-They are considered equal
-'''
-post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
-post_2 = Post(None, 'A test title', 'Test content', None, 0, 1)
-post_1 == post_2
+def test_post_inits_correctly():
+    '''
+    When I create a post
+    It initialises correctly
+    '''
+    post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
+    assert post_1.title == 'A test title'
+    assert post_1.contents == 'Test content'
+    assert post_1.views == 0
+    assert post_1.user_id == 1
 
-'''
-When the post object is converted to a string
-It pretty prints its attributes
-'''
-post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
-str(post_1) == Post(A test title, Test content, 1)
+def test_two_post_objects_are_identical():
+    '''
+    When I add two posts with identical attributes
+    They are considered equal
+    '''
+    post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
+    post_2 = Post(None, 'A test title', 'Test content', None, 0, 1)
+    assert post_1 == post_2
+
+def test_post_string_pretty_prints():
+    '''
+    When the post object is converted to a string
+    It pretty prints its attributes
+    '''
+    post_1 = Post(None, 'A test title', 'Test content', None, 0, 1)
+    assert str(post_1) == 'Post(None, A test title, Test content, None, 0, 1)'
