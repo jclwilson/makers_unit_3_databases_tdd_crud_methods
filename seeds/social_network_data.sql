05a6c7250e57ba9374310598a0f6ac74d7b5e5db
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 15:43:29 +0000

    Updates seed data

--- seeds/social_network_data.sql
+++ seeds/social_network_data.sql
@@ -3,3 +3,4 @@
 INSERT INTO posts (title, content, user_id) VALUES ('An example title', 'Hello, today I went to the shops.', 1);
 INSERT INTO posts (title, content, user_id) VALUES ('Swimming pool times', 'Hello, today I went to the swimming pool.', 3);
 INSERT INTO posts (title, content, user_id) VALUES ('Tech news', 'A long blog post about a particualr computing problem.', 2);
+INSERT INTO posts (title, content, user_id) VALUES ('Art news', 'About an exhibition I saw at the weekend.', 1);
--- seeds/social_network_users.sql
+++ seeds/social_network_users.sql
@@ -1,6 +1,6 @@
 -- Seed users table with example data
 
-INSERT INTO users (username, email_address) VALUES ('jake', 'jake@email.com');
-INSERT INTO users (username, email_address) VALUES ('imogen', 'imogen@email.com');
-INSERT INTO users (username, email_address) VALUES ('jack', 'jack@email.com');
-INSERT INTO users (username, email_address) VALUES ('alec', 'alec@email.com');
+INSERT INTO users (username, email_address) VALUES ('Jake', 'jake@email.com');
+INSERT INTO users (username, email_address) VALUES ('Imogen', 'imogen@email.com');
+INSERT INTO users (username, email_address) VALUES ('Jack', 'jack@email.com');
+INSERT INTO users (username, email_address) VALUES ('Alec', 'alec@email.com');
