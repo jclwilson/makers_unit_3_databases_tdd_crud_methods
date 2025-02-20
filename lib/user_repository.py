3eed56f19c08032401e57eb34fd2eca001c453ee
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 15:44:22 +0000

    Implements method for getting user info and post info object

--- lib/user_repository.py
+++ lib/user_repository.py
@@ -1,8 +1,10 @@
 '''
-UserRepository repository class
+UserRepository class implementing CRUD methods on Post class.
 '''
 
+from lib import post_repository
 from lib.user import User
+from lib.post_repository import PostRepository
 
 class UserRepository:
     def __init__(self, db_connection):
@@ -31,7 +33,13 @@ class UserRepository:
             return user
         else:
             return None
-    
+
     def delete(self, user_id):
         self._db_connection.execute("DELETE FROM users WHERE id = %s", [user_id])
         return None
+
+    def get_user_post_info(self, user_id):
+        post_repository = PostRepository(self._db_connection)
+        user = self.find(user_id)
+        user.posts = post_repository.by_user(user_id)
+        return user
