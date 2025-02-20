4699a1f76617c54168feaee39f6e332c59fb94dd
Jacob Wilson <hello@jacobcharleswilson.com>
2025-02-20 13:29:08 +0000

    Implements Post class

--- lib/post.py
+++ lib/post.py
@@ -2,13 +2,18 @@ class Post:
     '''
     Class that models a signle post.
     '''
-    def __init__(self, id, title, contents, publish_date, views):
+    def __init__(self, id, title, contents, publish_date, views, user_id):
         '''
         Parameters: id, title, contents, publish_date, views
         Side effects: None
         Returns: None
         '''
-        pass
+        self.id = id
+        self.title = title
+        self.contents = contents
+        self.publish_date = publish_date
+        self.views = views
+        self.user_id = user_id
 
     def __eq__(self, other):
         '''
@@ -16,7 +21,7 @@ class Post:
         Side effects: rewrites eq method
         Returns: statement asserting object instances with same attributes are equal.
         '''
-        pass
+        return self.__dict__ == other.__dict__
 
     def __repr__(self):
         '''
@@ -24,4 +29,4 @@ class Post:
         Side effects: rewrites repr method.
         Returns: Pretty printed representation of the instance object.
         '''
-        pass
+        return f"Post({self.id}, {self.title}, {self.contents}, {self.publish_date}, {self.views}, {self.user_id})"
